4c5f137787eae9d53759fd739adf8e25ec038233
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-01-17 15:59:04 -0500

    Create DetailPage UI screen

---
+++ lib/detail_page.ui.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_amiibo_responsive/model/amiibo.model.dart';
+import 'package:flutter_amiibo_responsive/utils/utilities.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({Key key, this.amiibo}) : super(key: key);
+
+  final AmiiboModel amiibo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          amiibo.name,
+          style: GoogleFonts.nunito(fontSize: 24),
+        ),
+      ),
+      body: ListView(
+        children: <Widget>[
+          _setImage(),
+          _setTitleSection(),
+          _setButtonSection(),
+          _setDescriptionSection(),
+        ],
+      ),
+    );
+  }
+
+  Widget _setImage() {
+    return Hero(
+      tag: '${amiibo.head}_${amiibo.tail}',
+      child: Image.network(
+        amiibo.imageUrl,
+        height: 350,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _setButtonSection() {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _setButtonItem(Icons.shopping_bag, 'Buy article', Colors.blueGrey),
+          _setButtonItem(Icons.favorite, 'Add favorite', Colors.blueGrey),
+          _setButtonItem(Icons.share, 'Share to...', Colors.blueGrey),
+        ],
+      ),
+    );
+  }
+
+  Widget _setButtonItem(IconData icon, String text, Color color) {
+    return InkWell(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(icon, color: color),
+            Container(
+              margin: const EdgeInsets.only(top: 8),
+              child: Text(
+                text.toUpperCase(),
+                style: GoogleFonts.nunito(
+                  fontSize: 12,
+                  color: color,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _setTitleSection() {
+    return Container(
+      padding: const EdgeInsets.all(24),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    amiibo.name,
+                    style: GoogleFonts.nunito(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  amiibo.amiiboSeries,
+                  style: GoogleFonts.nunito(
+                    color: Colors.grey[500],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Icon(
+            Icons.art_track,
+            color: Colors.blueGrey[500],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 4),
+            child: Text(
+              amiibo.type,
+              style: GoogleFonts.nunito(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _setDescriptionSection() {
+    return Padding(
+      padding: const EdgeInsets.all(24),
+      child: Text(
+        Utilities.setLoremText(),
+        style: GoogleFonts.nunito(
+          fontSize: 16,
+        ),
+      ),
+    );
+  }
+}
