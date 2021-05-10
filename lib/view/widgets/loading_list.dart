080ce2ffff6ba84004af7e82ed46e0598a96cfa0
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-05-09 22:39:17 -0500

    Create loading_list.dart using shimmer package

---
+++ lib/view/widgets/loading_list.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer_animation/shimmer_animation.dart';
+
+class LoadingList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final fakeList = List.filled(20, 0);
+
+    return GridView.extent(
+      maxCrossAxisExtent: size.width >= 600 ? 300 : 200,
+      mainAxisSpacing: 8,
+      crossAxisSpacing: 8,
+      padding: const EdgeInsets.all(8),
+      childAspectRatio: 1 / 1.2,
+      children: <Widget>[
+        for (var i = 0; i < fakeList.length; i++)
+          Shimmer(
+            direction: ShimmerDirection.fromLTRB(),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey.shade300,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              constraints: BoxConstraints(
+                minHeight: 200,
+                minWidth: MediaQuery.of(context).size.width * 0.9,
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+}
