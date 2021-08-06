5f191b42eb00a4dda3c17a2637a516c538b2a24c
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-08-06 11:25:52 -0500

    Rename loading_list.dart by shimmer_grid_loading.dart

--- lib/view/widgets/shimmer_grid_loading.dart
+++ lib/view/widgets/shimmer_grid_loading.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shimmer_animation/shimmer_animation.dart';
 
-class LoadingList extends StatelessWidget {
-  const LoadingList({Key? key}) : super(key: key);
+class ShimmerGridLoading extends StatelessWidget {
+  const ShimmerGridLoading({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
