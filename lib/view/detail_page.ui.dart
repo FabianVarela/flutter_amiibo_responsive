842940080dbd635fd9e8acb039935e4c9a3a6d86
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-08-06 11:25:29 -0500

    Refactor detail_page.ui.dart

--- lib/view/detail_page.ui.dart
+++ lib/view/detail_page.ui.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_amiibo_responsive/model/amiibo.model.dart';
 import 'package:flutter_amiibo_responsive/utils/utilities.dart';
+import 'package:flutter_amiibo_responsive/view/widgets/vertical_icon_button.dart';
 
 class DetailPage extends StatelessWidget {
   const DetailPage({Key? key, required this.amiibo}) : super(key: key);
@@ -16,132 +17,120 @@ class DetailPage extends StatelessWidget {
         title: Text(amiibo.name, style: const TextStyle(fontSize: 24)),
       ),
       body: OrientationBuilder(
-        builder: (_, orientation) =>
-            (size.width >= 600 || orientation == Orientation.landscape)
-                ? SingleChildScrollView(
-                    child: Padding(
-                      padding: const EdgeInsets.all(24),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Expanded(
-                            child: Column(
-                              children: <Widget>[
-                                _setImage(),
-                                _setTitleSection(),
-                              ],
-                            ),
+        builder: (_, orientation) {
+          return (size.width >= 600 || orientation == Orientation.landscape)
+              ? SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(24),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Expanded(
+                          child: Column(
+                            children: <Widget>[
+                              _setImage(),
+                              _setTitleSection(),
+                            ],
                           ),
-                          Expanded(
-                            child: Column(
-                              children: <Widget>[
-                                _setButtonSection(),
-                                _setDescriptionSection(),
-                              ],
-                            ),
+                        ),
+                        Expanded(
+                          child: Column(
+                            children: <Widget>[
+                              _setButtonSection(),
+                              _setDescriptionSection(),
+                            ],
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  )
-                : ListView(
-                    children: <Widget>[
-                      _setImage(),
-                      _setTitleSection(),
-                      _setButtonSection(),
-                      _setDescriptionSection(),
-                    ],
                   ),
+                )
+              : ListView(
+                  children: <Widget>[
+                    _setImage(),
+                    _setTitleSection(),
+                    _setButtonSection(),
+                    _setDescriptionSection(),
+                  ],
+                );
+        },
       ),
     );
   }
 
-  Widget _setImage() => Hero(
-        tag: '${amiibo.head}_${amiibo.tail}',
-        child: Image.network(
-          amiibo.imageUrl,
-          height: 350,
-          fit: BoxFit.cover,
-        ),
-      );
+  Widget _setImage() {
+    return Hero(
+      tag: '${amiibo.head}_${amiibo.tail}',
+      child: Image.network(amiibo.imageUrl, height: 350, fit: BoxFit.cover),
+    );
+  }
 
-  Widget _setButtonSection() => Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  Widget _setButtonSection() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: const <Widget>[
+        VerticalIconButton(
+          icon: Icons.shopping_bag,
+          text: 'Buy article',
+          color: Colors.blueGrey,
+        ),
+        VerticalIconButton(
+          icon: Icons.favorite,
+          text: 'Add favorite',
+          color: Colors.blueGrey,
+        ),
+        VerticalIconButton(
+          icon: Icons.share,
+          text: 'Share to...',
+          color: Colors.blueGrey,
+        ),
+      ],
+    );
+  }
+
+  Widget _setTitleSection() {
+    return Padding(
+      padding: const EdgeInsets.all(24),
+      child: Row(
         children: <Widget>[
-          _setButtonItem(Icons.shopping_bag, 'Buy article', Colors.blueGrey),
-          _setButtonItem(Icons.favorite, 'Add favorite', Colors.blueGrey),
-          _setButtonItem(Icons.share, 'Share to...', Colors.blueGrey),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    amiibo.name,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(
+                  amiibo.amiiboSeries,
+                  style: TextStyle(color: Colors.grey[500]),
+                )
+              ],
+            ),
+          ),
+          Icon(Icons.art_track, color: Colors.blueGrey[500]),
+          Padding(
+            padding: const EdgeInsets.only(left: 4),
+            child: Text(
+              amiibo.type,
+              style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+            ),
+          )
         ],
-      );
-
-  Widget _setButtonItem(IconData icon, String text, Color color) {
-    return InkWell(
-      onTap: () {},
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(icon, color: color),
-            Container(
-              margin: const EdgeInsets.only(top: 8),
-              child: Text(
-                text.toUpperCase(),
-                style: TextStyle(fontSize: 12, color: color),
-              ),
-            )
-          ],
-        ),
       ),
     );
   }
 
-  Widget _setTitleSection() => Container(
-        padding: const EdgeInsets.all(24),
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.only(bottom: 8),
-                    child: Text(
-                      amiibo.name,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Text(
-                    amiibo.amiiboSeries,
-                    style: TextStyle(color: Colors.grey[500]),
-                  )
-                ],
-              ),
-            ),
-            Icon(
-              Icons.art_track,
-              color: Colors.blueGrey[500],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 4),
-              child: Text(
-                amiibo.type,
-                style: const TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            )
-          ],
-        ),
-      );
-
-  Widget _setDescriptionSection() => Padding(
-        padding: const EdgeInsets.all(24),
-        child: Text(
-          Utilities.setLoremText(),
-          style: const TextStyle(fontSize: 16),
-        ),
-      );
+  Widget _setDescriptionSection() {
+    return Padding(
+      padding: const EdgeInsets.all(24),
+      child: Text(
+        Utilities.setLoremText(),
+        style: const TextStyle(fontSize: 16),
+      ),
+    );
+  }
 }
