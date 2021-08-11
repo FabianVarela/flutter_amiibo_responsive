7ee4c60a8a309ea19c36815a0aec7047034692d2
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-08-11 14:38:30 -0500

    Modify main.dart replacing home widget by home router

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_amiibo_responsive/view/home.ui.dart';
+import 'package:flutter_amiibo_responsive/navigator/router_delegate.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,11 +21,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
-        textTheme: GoogleFonts.nunitoTextTheme(
-          Theme.of(context).textTheme,
-        ),
+        textTheme: GoogleFonts.nunitoTextTheme(Theme.of(context).textTheme),
       ),
-      home: const HomePageUI(),
+      home: Router<dynamic>(
+        routerDelegate: AmiiboRouterDelegate(),
+        backButtonDispatcher: RootBackButtonDispatcher(),
+      ),
     );
   }
 }
