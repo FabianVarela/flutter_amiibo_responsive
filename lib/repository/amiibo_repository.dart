b1db825c9447bd6a0c9f8e20efd053560ca4369b
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-08-11 22:36:06 -0500

    Create amiibo_repository.dart to connect with client

---
+++ lib/repository/amiibo_repository.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_amiibo_responsive/client/amiibo_client.dart';
+import 'package:flutter_amiibo_responsive/model/amiibo_model.dart';
+
+class AmiiboRepository {
+  AmiiboRepository(this._amiiboClient);
+
+  final AmiiboClient _amiiboClient;
+
+  Future<List<AmiiboModel>> getAmiiboList(String? param) =>
+      _amiiboClient.getAmiiboList(param);
+}
