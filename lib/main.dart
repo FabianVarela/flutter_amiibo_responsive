c7bcfe3dd1da66ec90d88082f375e32689e44176
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-08-11 22:39:25 -0500

    Update main.dart adding bloc provider and set route delegate as variable

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_amiibo_responsive/bloc/amiibo_cubit.dart';
+import 'package:flutter_amiibo_responsive/client/amiibo_client.dart';
 import 'package:flutter_amiibo_responsive/navigator/router_delegate.dart';
+import 'package:flutter_amiibo_responsive/repository/amiibo_repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,18 +19,23 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _routerDelegate = AmiiboRouterDelegate();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Amiibo Responsive App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        textTheme: GoogleFonts.nunitoTextTheme(Theme.of(context).textTheme),
-      ),
-      home: Router<dynamic>(
-        routerDelegate: AmiiboRouterDelegate(),
-        backButtonDispatcher: RootBackButtonDispatcher(),
+    return BlocProvider(
+      create: (_) => AmiiboCubit(AmiiboRepository(AmiiboClient(Client()))),
+      child: MaterialApp(
+        title: 'Amiibo Responsive App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          textTheme: GoogleFonts.nunitoTextTheme(Theme.of(context).textTheme),
+        ),
+        home: Router<dynamic>(
+          routerDelegate: _routerDelegate,
+          backButtonDispatcher: RootBackButtonDispatcher(),
+        ),
       ),
     );
   }
