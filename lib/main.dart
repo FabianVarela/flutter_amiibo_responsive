5a68585684d77daf06736155d6f806387f57c668
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-08-28 17:26:19 -0500

    Rename files

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_amiibo_responsive/bloc/amiibo_item/amiibo_item_cubit.dart';
 import 'package:flutter_amiibo_responsive/bloc/amiibo_list/amiibo_list_cubit.dart';
 import 'package:flutter_amiibo_responsive/client/amiibo_client.dart';
-import 'package:flutter_amiibo_responsive/navigator/router_delegate.dart';
+import 'package:flutter_amiibo_responsive/navigator/amiibo_router_delegate.dart';
 import 'package:flutter_amiibo_responsive/repository/amiibo_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
--- lib/navigator/amiibo_router_delegate.dart
+++ lib/navigator/amiibo_router_delegate.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_amiibo_responsive/navigator/page.dart';
+import 'package:flutter_amiibo_responsive/navigator/amiibo_pages.dart';
 
 class AmiiboRouterDelegate extends RouterDelegate<Object>
     with ChangeNotifier, PopNavigatorRouterDelegateMixin<Object> {
