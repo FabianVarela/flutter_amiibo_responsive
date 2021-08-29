f9014f7445cef0ef2e60bad4fa95c48ccc2a4409
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-08-29 18:41:43 -0500

    Create unknown_page_ui.dart for not found

---
+++ lib/view/unknown_page_ui.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class UnknownPageUI extends StatelessWidget {
+  const UnknownPageUI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: const Text(
+          'Not found',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+      body: SizedBox(
+        width: size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/images/mario 404.png',
+              fit: BoxFit.contain,
+              height: size.height * 0.4,
+              width: size.width * 0.4,
+            ),
+            const SizedBox(height: 50),
+            const Text(
+              'Oops!!! Page not found',
+              style: TextStyle(fontSize: 30),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
