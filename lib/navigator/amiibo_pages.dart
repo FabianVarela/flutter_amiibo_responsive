d73f5f6c9044f9b05ac562534eb07ab84fbb502f
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-08-29 19:07:20 -0500

    Update amiibo_pages.dart adding unknown pages

--- lib/navigator/amiibo_pages.dart
+++ lib/navigator/amiibo_pages.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_amiibo_responsive/view/detail_page_ui.dart';
 import 'package:flutter_amiibo_responsive/view/home_ui.dart';
+import 'package:flutter_amiibo_responsive/view/unknown_page_ui.dart';
 
 class HomePage extends Page<dynamic> {
   HomePage({
@@ -27,9 +28,10 @@ class HomePage extends Page<dynamic> {
 }
 
 class DetailPage extends Page<dynamic> {
-  const DetailPage({required this.amiiboId})
+  const DetailPage({this.type, required this.amiiboId})
       : super(key: const ValueKey('DetailPage'));
 
+  final String? type;
   final String amiiboId;
 
   @override
@@ -38,7 +40,20 @@ class DetailPage extends Page<dynamic> {
       settings: this,
       pageBuilder: (_, animation, __) => FadeTransition(
         opacity: animation,
-        child: DetailPageUI(amiiboId: amiiboId),
+        child: DetailPageUI(type: type, amiiboId: amiiboId),
+      ),
+    );
+  }
+}
+
+class UnknownPage extends Page<dynamic> {
+  @override
+  Route createRoute(BuildContext context) {
+    return PageRouteBuilder<dynamic>(
+      settings: this,
+      pageBuilder: (_, animation, __) => ScaleTransition(
+        scale: animation,
+        child: const UnknownPageUI(),
       ),
     );
   }
