e4c67b853186c31cd7acba065202a19517b0591d
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-09-11 22:45:34 -0500

    Create fake data in params_factory.dart

---
+++ test/mock/params_factory.dart
@@ -0,0 +1,19 @@
+const String jsonListResponse =
+    '{ "amiibo": [ { "amiiboSeries": "Legend Of Zelda", '
+    '"character": "Zelda", "gameSeries": "The Legend of Zelda", '
+    '"head": "01010000", "image": "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_01010000-03520902.png", '
+    '"name": "Toon Zelda - The Wind Waker", "release": { "au": "2016-12-03", '
+    '"eu": "2016-12-02", "jp": "2016-12-01", "na": "2016-12-02" }, '
+    '"tail": "03520902", "type": "Figure" } ] }';
+
+const String jsonDetailResponse =
+    '{ "amiibo": { "amiiboSeries": "Legend Of Zelda", '
+    '"character": "Zelda", "gameSeries": "The Legend of Zelda", '
+    '"head": "01010000", "image": "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_01010000-03520902.png", '
+    '"name": "Toon Zelda - The Wind Waker", "release": { "au": "2016-12-03", '
+    '"eu": "2016-12-02", "jp": "2016-12-01", "na": "2016-12-02" }, '
+    '"tail": "03520902", "type": "Figure"} }';
+
+const amiiboType = 'figure';
+
+const amiiboId = '01010000000e0002';
