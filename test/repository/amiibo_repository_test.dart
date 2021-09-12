b252721be814dc884c79e79fb09ad5db13d1b1a3
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-09-11 23:24:51 -0500

    Add test methods for repository in amiibo_repository_test.dart

---
+++ test/repository/amiibo_repository_test.dart
@@ -0,0 +1,71 @@
+import 'package:flutter_amiibo_responsive/model/amiibo_model.dart';
+import 'package:flutter_amiibo_responsive/repository/amiibo_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../mock/mocks.dart';
+import '../mock/params_factory.dart';
+
+void main() {
+  late MockAmiiboClient mockAmiiboClient;
+  late AmiiboRepository amiiboRepository;
+
+  setUpAll(() {
+    mockAmiiboClient = MockAmiiboClient();
+    amiiboRepository = AmiiboRepository(mockAmiiboClient);
+
+    registerFallbackValue(MyAmiiboFake());
+  });
+
+  group('$AmiiboRepository', () {
+    test(
+        'should get a future with a $List of $AmiiboModel '
+        'when getAmiiboList is called', () async {
+      // arrange
+      final valueList = getAmiiboModel();
+      final futureValueList = Future.value([valueList]);
+
+      when(() => mockAmiiboClient.getAmiiboList(any()))
+          .thenAnswer((_) => futureValueList);
+
+      // act
+      final result = amiiboRepository.getAmiiboList(null);
+      final listResult = await result;
+
+      // assert
+      expect(result, isA<Future<List<AmiiboModel>>>());
+      expect(result, equals(futureValueList));
+
+      expect(listResult, isA<List<AmiiboModel>>());
+      expect(listResult, equals([valueList]));
+
+      verify(() => mockAmiiboClient.getAmiiboList(any())).called(1);
+      verifyNoMoreInteractions(mockAmiiboClient);
+    });
+
+    test(
+        'should get a future with an $AmiiboModel detail '
+        'when getAmiiboItem is called', () async {
+      // arrange
+      final value = getAmiiboModel();
+      final futureValue = Future.value(value);
+
+      when(() => mockAmiiboClient.getAmiiboItem(any(), any()))
+          .thenAnswer((_) => futureValue);
+
+      // act
+      final result = amiiboRepository.getAmiiboItem(amiiboType, amiiboId);
+      final valueResult = await result;
+
+      // assert
+      expect(result, isA<Future<AmiiboModel>>());
+      expect(result, equals(futureValue));
+
+      expect(valueResult, isA<AmiiboModel>());
+      expect(valueResult, equals(value));
+
+      verify(() => mockAmiiboClient.getAmiiboItem(any(), any())).called(1);
+      verifyNoMoreInteractions(mockAmiiboClient);
+    });
+  });
+}
