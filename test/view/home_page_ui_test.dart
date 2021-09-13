d35ba79356741dfa1afb18b2c509c085a3250cfc
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-09-12 20:59:10 -0500

    Create widget test methods for HomePageUI in home_page_ui_test.dart

---
+++ test/view/home_page_ui_test.dart
@@ -0,0 +1,153 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_amiibo_responsive/bloc/amiibo_list/amiibo_list_cubit.dart';
+import 'package:flutter_amiibo_responsive/bloc/amiibo_list/amiibo_list_state.dart';
+import 'package:flutter_amiibo_responsive/repository/amiibo_repository.dart';
+import 'package:flutter_amiibo_responsive/view/home_page_ui.dart';
+import 'package:flutter_amiibo_responsive/view/widgets/amiibo_item.dart';
+import 'package:flutter_amiibo_responsive/view/widgets/drawer_menu.dart';
+import 'package:flutter_amiibo_responsive/view/widgets/shimmer_grid_loading.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:network_image_mock/network_image_mock.dart';
+
+import '../mock/mocks.dart';
+import '../mock/params_factory.dart';
+
+void main() {
+  group('$HomePageUI UI screen', () {
+    late MockAmiiboClient mockAmiiboClient;
+    late AmiiboRepository amiiboRepository;
+    late AmiiboListCubit amiiboListCubit;
+
+    late MockNavigator mockNavigator;
+
+    setUpAll(() {
+      HttpOverrides.global = null;
+
+      mockAmiiboClient = MockAmiiboClient();
+      amiiboRepository = AmiiboRepository(mockAmiiboClient);
+      amiiboListCubit = AmiiboListCubit(amiiboRepository);
+
+      registerFallbackValue(const AmiiboListState());
+      registerFallbackValue(MyAmiiboFake());
+
+      mockNavigator = MockNavigator();
+      registerFallbackValue(MyRouteFake());
+    });
+
+    testWidgets('Show $HomePageUI screen with data', (tester) async {
+      final model = getAmiiboModel();
+      when(() => amiiboRepository.getAmiiboList(any())).thenAnswer(
+        (_) => Future.delayed(
+          const Duration(milliseconds: 100),
+          () => Future.value([model]),
+        ),
+      );
+
+      await mockNetworkImagesFor(() {
+        return tester.pumpWidget(MultiBlocProvider(
+          providers: [BlocProvider.value(value: amiiboListCubit)],
+          child: MaterialApp(
+            navigatorObservers: [mockNavigator],
+            home: HomePageUI(onChangeType: (_) {}, onGoToDetail: (_) {}),
+          ),
+        ));
+      });
+
+      final finderAppBar = find.byType(AppBar);
+      final finderText = find.text('Amiibo App');
+
+      expect(find.byType(DrawerMenu), findsOneWidget);
+      expect(
+        find.descendant(of: finderAppBar, matching: finderText),
+        findsOneWidget,
+      );
+      expect(find.byType(ShimmerGridLoading), findsOneWidget);
+
+      await tester.pumpAndSettle();
+
+      expect(find.byType(ShimmerGridLoading), findsNothing);
+      expect(find.byType(GridView), findsOneWidget);
+
+      expect(tester.widgetList(find.byType(AmiiboItem)), [
+        isA<AmiiboItem>()
+            .having((w) => w.amiibo.name, 'name', model.name)
+            .having((w) => w.amiibo.type, 'type', model.type)
+            .having((w) => w.amiibo.imageUrl, 'imageUrl', model.imageUrl)
+            .having((w) => w.amiibo.character, 'character', model.character)
+            .having((w) => w.amiibo.gameSeries, 'gameSeries', model.gameSeries),
+      ]);
+    });
+
+    testWidgets('Show $HomePageUI screen with empty data', (tester) async {
+      when(() => amiiboRepository.getAmiiboList(any())).thenAnswer(
+        (_) => Future.delayed(
+          const Duration(milliseconds: 100),
+          () => Future.value([]),
+        ),
+      );
+
+      await mockNetworkImagesFor(() {
+        return tester.pumpWidget(MultiBlocProvider(
+          providers: [BlocProvider.value(value: amiiboListCubit)],
+          child: MaterialApp(
+            navigatorObservers: [mockNavigator],
+            home: HomePageUI(onChangeType: (_) {}, onGoToDetail: (_) {}),
+          ),
+        ));
+      });
+
+      final finderAppBar = find.byType(AppBar);
+      final finderText = find.text('Amiibo App');
+
+      expect(find.byType(DrawerMenu), findsOneWidget);
+      expect(
+        find.descendant(of: finderAppBar, matching: finderText),
+        findsOneWidget,
+      );
+      expect(find.byType(ShimmerGridLoading), findsOneWidget);
+
+      await tester.pumpAndSettle();
+
+      expect(find.byType(ShimmerGridLoading), findsNothing);
+      expect(find.text('No data found'), findsOneWidget);
+    });
+
+    testWidgets('Show $HomePageUI screen with $Exception', (tester) async {
+      when(() => amiiboRepository.getAmiiboList(any())).thenAnswer(
+        (_) => Future.delayed(
+          const Duration(milliseconds: 100),
+          () => throw Exception(),
+        ),
+      );
+
+      await mockNetworkImagesFor(() {
+        return tester.pumpWidget(BlocProvider<AmiiboListCubit>(
+          create: (_) => amiiboListCubit,
+          child: MaterialApp(
+            navigatorObservers: [mockNavigator],
+            home: HomePageUI(onChangeType: (_) {}, onGoToDetail: (_) {}),
+          ),
+        ));
+      });
+
+      final finderAppBar = find.byType(AppBar);
+      final finderText = find.text('Amiibo App');
+
+      expect(find.byType(DrawerMenu), findsOneWidget);
+      expect(
+        find.descendant(of: finderAppBar, matching: finderText),
+        findsOneWidget,
+      );
+      expect(find.byType(ShimmerGridLoading), findsOneWidget);
+
+      await tester.pumpAndSettle();
+
+      expect(find.byType(ShimmerGridLoading), findsNothing);
+      expect(find.text('Error to get data'), findsOneWidget);
+    });
+  });
+}
