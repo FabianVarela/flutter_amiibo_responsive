712026cbfd692623c1681e8b58c43f2c24079f33
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-09-13 20:12:54 -0500

    Add window size configuration for desktop apps

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_amiibo_responsive/bloc/amiibo_item/amiibo_item_cubit.dart';
 import 'package:flutter_amiibo_responsive/bloc/amiibo_list/amiibo_list_cubit.dart';
@@ -8,8 +10,17 @@ import 'package:flutter_amiibo_responsive/repository/amiibo_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart';
+import 'package:window_size/window_size.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle('Flutter Amiibo');
+    setWindowMinSize(const Size(800, 500));
+    setWindowMaxSize(Size.infinite);
+  }
+
   runApp(const MyApp());
 }
 
@@ -40,7 +51,7 @@ class _MyAppState extends State<MyApp> {
         )
       ],
       child: MaterialApp.router(
-        title: 'Amiibo Responsive App',
+        title: 'Flutter Amiibo App',
         theme: ThemeData(
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
