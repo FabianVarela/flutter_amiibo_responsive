de463a3bd39ce0ac6e5573777f510deef93ddc65
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-12-19 13:38:06 -0500

    Create bootstrap.dart to initialize flutter app

---
+++ lib/bootstrap.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:window_size/window_size.dart';
+
+class AppBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    log('onChange(${bloc.runtimeType}, $change)');
+  }
+
+  @override
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    log('onError(${bloc.runtimeType}, $error, $stackTrace)');
+    super.onError(bloc, error, stackTrace);
+  }
+}
+
+Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+  final desktopList = [
+    TargetPlatform.linux,
+    TargetPlatform.macOS,
+    TargetPlatform.windows,
+  ];
+
+  if (desktopList.contains(defaultTargetPlatform)) {
+    setWindowTitle('Flutter Amiibo');
+    setWindowMinSize(const Size(800, 500));
+    setWindowMaxSize(Size.infinite);
+  }
+
+  FlutterError.onError = (details) {
+    log(details.exceptionAsString(), stackTrace: details.stack);
+  };
+
+  Bloc.observer = AppBlocObserver();
+
+  await runZonedGuarded(
+    () async => runApp(await builder()),
+    (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
+  );
+}
