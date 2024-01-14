ae60490541047058f9545ba34ed00eb6db516da6
FabianVarela <jfabian.303@gmail.com>
2024-01-13 19:36:20 -0500

    update bash command files

--- test_coverage.ps1
+++ test_coverage.ps1
@@ -1,5 +1,5 @@
 flutter test --coverage
-flutter pub run remove_from_coverage -f coverage/lcov.info -r '\.g\.dart$'
-flutter pub run remove_from_coverage -f coverage/lcov.info -r '\.freezed\.dart$'
+dart run remove_from_coverage -f coverage/lcov.info -r '\.g\.dart$'
+dart run remove_from_coverage -f coverage/lcov.info -r '\.freezed\.dart$'
 perl C:\ProgramData\chocolatey\lib\lcov\tools\bin\genhtml -o coverage\html coverage\lcov.info
 coverage/html/index.html
