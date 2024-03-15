fc58009e714676ac7f141e5131ccde3561bcba20
FabianVarela <jfabian.303@gmail.com>
2024-03-15 11:36:24 -0500

    Become to enhanced enum

--- lib/utils/enum.dart
+++ lib/utils/enum.dart
@@ -1,6 +1,15 @@
+import 'package:flutter/material.dart';
+
 enum AmiiboType {
-  figure,
-  card,
-  yarn,
-  band,
+  all(null, 'All', Icons.list),
+  figure('figure', 'Figure', Icons.account_box),
+  card('card', 'Card', Icons.card_membership),
+  yarn('yarn', 'Yarn', Icons.wallpaper),
+  band('band', 'Band', Icons.watch);
+
+  const AmiiboType(this.value, this.text, this.icon);
+
+  final String? value;
+  final String text;
+  final IconData icon;
 }
