abb99f18231cbf8725f6005ab0dafab59a14efc1
FabianVarela <jfabian.303@gmail.com>
2024-05-28 21:21:32 -0500

    Modify app_links implementations

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 
+import 'package:app_links/app_links.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_amiibo_responsive/client/amiibo_client.dart';
 import 'package:flutter_amiibo_responsive/navigator/amiibo_information_parser.dart';
 import 'package:flutter_amiibo_responsive/navigator/amiibo_router_delegate.dart';
@@ -10,9 +10,6 @@ import 'package:flutter_amiibo_responsive/repository/amiibo_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart';
-import 'package:uni_links/uni_links.dart';
-
-bool _initialUriLink = false;
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -25,13 +22,13 @@ class MyAppState extends State<MyApp> {
   final _routerDelegate = AmiiboRouterDelegate();
   final _informationParser = AmiiboInfoParser();
 
+  final _appLinks = AppLinks();
   StreamSubscription<Uri?>? _linkSubscription;
 
   @override
   void initState() {
     super.initState();
-    _initUri();
-    if (!kIsWeb) _incomingUri();
+    _initDeepLinks();
   }
 
   @override
@@ -69,30 +66,9 @@ class MyAppState extends State<MyApp> {
     );
   }
 
-  Future<void> _initUri() async {
-    if (!_initialUriLink) {
-      _initialUriLink = true;
-
-      try {
-        final initialUri = await getInitialUri();
-        if (initialUri != null) {
-          _routerDelegate.parseRoute(initialUri);
-        }
-      } on PlatformException {
-        if (kDebugMode) print('Failed to receive Uri');
-      } on FormatException catch (_) {
-        if (kDebugMode) print('Failed in format initial Uri');
-      }
-    }
-  }
-
-  Future<void> _incomingUri() async {
-    _linkSubscription = uriLinkStream.listen(
-      (uri) {
-        if (uri != null) {
-          _routerDelegate.parseRoute(uri);
-        }
-      },
+  Future<void> _initDeepLinks() async {
+    _linkSubscription = _appLinks.uriLinkStream.listen(
+      _routerDelegate.parseRoute,
       onError: (Object error) {
         if (kDebugMode) print('Uri incoming error: $error');
         if (error is FormatException) {
