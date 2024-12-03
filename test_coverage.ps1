c30852e726dedf895fce08e1361b43a96128f197
FabianVarela <jfabian.303@gmail.com>
2024-12-02 23:23:44 -0500

    Modify test coverage files

--- test_coverage.ps1
+++ test_coverage.ps1
@@ -1,4 +1,4 @@
-flutter test --coverage
+flutter test --concurrency 4 --coverage --test-randomize-ordering-seed random
 dart run remove_from_coverage -f coverage/lcov.info -r '\.g\.dart$'
 dart run remove_from_coverage -f coverage/lcov.info -r '\.freezed\.dart$'
 perl C:\ProgramData\chocolatey\lib\lcov\tools\bin\genhtml -o coverage\html coverage\lcov.info
