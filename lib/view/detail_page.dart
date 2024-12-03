dd0947b0b8c1894398e730b12582fafaceaf3bf0
FabianVarela <jfabian.303@gmail.com>
2024-12-03 17:52:29 -0500

    Improve detail page setting adaptive breakpoints

--- lib/view/detail_page.dart
+++ lib/view/detail_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_amiibo_responsive/bloc/amiibo_item/amiibo_item_cubit.dart';
 import 'package:flutter_amiibo_responsive/model/amiibo_model.dart';
 import 'package:flutter_amiibo_responsive/repository/amiibo_repository.dart';
+import 'package:flutter_amiibo_responsive/utils/adaptive_contextual_layout.dart';
 import 'package:flutter_amiibo_responsive/view/widgets/vertical_icon_button.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -29,7 +30,9 @@ class DetailView extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.sizeOf(context).width;
+    final isDesktopOrTablet = [ScreenType.desktop, ScreenType.tablet].contains(
+      context.formFactor,
+    );
 
     useEffect(
       () {
@@ -58,45 +61,34 @@ class DetailView extends HookWidget {
       body: BlocBuilder<AmiiboItemCubit, AmiiboItemState>(
         builder: (_, state) {
           if (state is AmiiboItemStateSuccess) {
-            return OrientationBuilder(
-              builder: (_, orientation) {
-                return (width >= 600 || orientation == Orientation.landscape)
-                    ? SingleChildScrollView(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 24,
-                          horizontal: 16,
+            return SingleChildScrollView(
+              padding: isDesktopOrTablet
+                  ? const EdgeInsets.symmetric(vertical: 24, horizontal: 16)
+                  : null,
+              child: isDesktopOrTablet
+                  ? Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Expanded(
+                          child: _AmiiboDetail(item: state.amiiboItem),
                         ),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Expanded(
-                              child: Column(
-                                children: <Widget>[
-                                  _AmiiboImage(item: state.amiiboItem),
-                                  _AmiiboDetail(item: state.amiiboItem),
-                                ],
-                              ),
-                            ),
-                            const Expanded(
-                              child: Column(
-                                children: <Widget>[
-                                  _AmiiboButtons(),
-                                  _AmiiboDescription(),
-                                ],
-                              ),
-                            ),
-                          ],
+                        const Expanded(
+                          child: Column(
+                            children: <Widget>[
+                              _AmiiboButtons(),
+                              _AmiiboDescription(),
+                            ],
+                          ),
                         ),
-                      )
-                    : ListView(
-                        children: <Widget>[
-                          _AmiiboImage(item: state.amiiboItem),
-                          _AmiiboDetail(item: state.amiiboItem),
-                          const _AmiiboButtons(),
-                          const _AmiiboDescription(),
-                        ],
-                      );
-              },
+                      ],
+                    )
+                  : Column(
+                      children: <Widget>[
+                        _AmiiboDetail(item: state.amiiboItem),
+                        const _AmiiboButtons(),
+                        const _AmiiboDescription(),
+                      ],
+                    ),
             );
           }
 
@@ -118,24 +110,6 @@ class DetailView extends HookWidget {
   }
 }
 
-class _AmiiboImage extends StatelessWidget {
-  const _AmiiboImage({required this.item});
-
-  final AmiiboModel item;
-
-  @override
-  Widget build(BuildContext context) {
-    return Hero(
-      tag: '${item.head}_${item.tail}',
-      child: Image.network(
-        item.imageUrl,
-        height: 350,
-        fit: BoxFit.cover,
-      ),
-    );
-  }
-}
-
 class _AmiiboDetail extends StatelessWidget {
   const _AmiiboDetail({required this.item});
 
@@ -143,38 +117,53 @@ class _AmiiboDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(24),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    item.name,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
+    return Column(
+      children: <Widget>[
+        Hero(
+          tag: '${item.head}_${item.tail}',
+          child: Image.network(
+            item.imageUrl,
+            height: 350,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(24),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 8),
+                      child: Text(
+                        item.name,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Text(
+                      item.amiiboSeries,
+                      style: TextStyle(color: Colors.grey[500]),
+                    ),
+                  ],
+                ),
+              ),
+              Icon(Icons.art_track, color: Colors.blueGrey[500]),
+              Padding(
+                padding: const EdgeInsets.only(left: 4),
+                child: Text(
+                  item.type,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                Text(
-                  item.amiiboSeries,
-                  style: TextStyle(color: Colors.grey[500]),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-          Icon(Icons.art_track, color: Colors.blueGrey[500]),
-          Padding(
-            padding: const EdgeInsets.only(left: 4),
-            child: Text(
-              item.type,
-              style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
