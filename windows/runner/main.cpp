dca1513246ba744baa7f4791ed2f8974fb50198a
FabianVarela <jfabian.303@gmail.com>
2025-01-04 23:38:58 -0500

    Fix windows implementations

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -69,7 +69,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"Amiibo App", origin, size)) {
+  if (!window.CreateAndShow(L"Amiibo App", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
