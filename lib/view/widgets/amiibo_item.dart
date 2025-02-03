64b5a27ef1921e07b6504e5d29aa0baa5bd8c11d
FabianVarela <jfabian.303@gmail.com>
2025-02-02 21:26:39 -0500

    Replace GridTile widget

--- lib/view/widgets/amiibo_item.dart
+++ lib/view/widgets/amiibo_item.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_amiibo_responsive/model/amiibo_model.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:gap/gap.dart';
 
 final class AmiiboItem extends StatelessWidget {
   const AmiiboItem({
@@ -20,24 +20,40 @@ final class AmiiboItem extends StatelessWidget {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
       child: InkWell(
         onTap: onSelectAmiibo,
-        child: GridTile(
-          footer: GridTileBar(
-            backgroundColor: colorScheme.surfaceContainerHighest.withValues(
-              alpha: .6,
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Hero(
+              tag: '${amiibo.head}_${amiibo.tail}',
+              child: Image.network(amiibo.imageUrl, fit: BoxFit.cover),
             ),
-            title: Text(
-              amiibo.name,
-              style: GoogleFonts.nunito(fontSize: 16),
+            Positioned(
+              bottom: 0,
+              width: MediaQuery.sizeOf(context).width,
+              child: ColoredBox(
+                color: colorScheme.surfaceContainerHighest.withValues(
+                  alpha: .6,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        amiibo.name,
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      const Gap(2),
+                      Text(
+                        amiibo.gameSeries ?? 'Sin serie',
+                        style: const TextStyle(fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-            subtitle: Text(
-              amiibo.gameSeries ?? 'Sin serie',
-              style: GoogleFonts.nunito(fontSize: 14),
-            ),
-          ),
-          child: Hero(
-            tag: '${amiibo.head}_${amiibo.tail}',
-            child: Image.network(amiibo.imageUrl, fit: BoxFit.cover),
-          ),
+          ],
         ),
       ),
     );
