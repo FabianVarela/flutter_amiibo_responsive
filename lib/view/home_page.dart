17ad599e4738a2d9b9794eca97c9bc1f569f076c
FabianVarela <jfabian.303@gmail.com>
2026-03-01 14:33:12 -0500

    Set filter chip and improve design and code

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -3,11 +3,13 @@ import 'dart:async';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_amiibo_responsive/bloc/amiibo_list/amiibo_list_cubit.dart';
+import 'package:flutter_amiibo_responsive/bloc/amiibo_series/amiibo_series_cubit.dart';
 import 'package:flutter_amiibo_responsive/bloc/game_series/game_series_cubit.dart';
 import 'package:flutter_amiibo_responsive/repository/amiibo_repository.dart';
 import 'package:flutter_amiibo_responsive/utils/adaptive_contextual_layout.dart';
 import 'package:flutter_amiibo_responsive/view/widgets/amiibo_item.dart';
 import 'package:flutter_amiibo_responsive/view/widgets/drawer_menu.dart';
+import 'package:flutter_amiibo_responsive/view/widgets/filter_chips.dart';
 import 'package:flutter_amiibo_responsive/view/widgets/shimmer_grid_loading.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -16,16 +18,20 @@ final class HomePage extends StatelessWidget {
   const HomePage({
     required this.onChangeType,
     required this.onChangeGameSeries,
+    required this.onChangeAmiiboSeries,
     required this.onGoToDetail,
     this.type,
     this.gameSeries,
+    this.amiiboSeries,
     super.key,
   });
 
   final String? type;
   final String? gameSeries;
+  final String? amiiboSeries;
   final ValueSetter<String?> onChangeType;
   final ValueSetter<String?> onChangeGameSeries;
+  final ValueSetter<String?> onChangeAmiiboSeries;
   final ValueSetter<String> onGoToDetail;
 
   @override
@@ -38,12 +44,17 @@ final class HomePage extends StatelessWidget {
         BlocProvider(
           create: (_) => GameSeriesCubit(context.read<AmiiboRepository>()),
         ),
+        BlocProvider(
+          create: (_) => AmiiboSeriesCubit(context.read<AmiiboRepository>()),
+        ),
       ],
       child: HomePageView(
         type: type,
         gameSeries: gameSeries,
+        amiiboSeries: amiiboSeries,
         onChangeType: onChangeType,
         onChangeGameSeries: onChangeGameSeries,
+        onChangeAmiiboSeries: onChangeAmiiboSeries,
         onGoToDetail: onGoToDetail,
       ),
     );
@@ -54,16 +65,20 @@ final class HomePageView extends HookWidget {
   const HomePageView({
     required this.onChangeType,
     required this.onChangeGameSeries,
+    required this.onChangeAmiiboSeries,
     required this.onGoToDetail,
     this.type,
     this.gameSeries,
+    this.amiiboSeries,
     super.key,
   });
 
   final String? type;
   final String? gameSeries;
+  final String? amiiboSeries;
   final ValueSetter<String?> onChangeType;
   final ValueSetter<String?> onChangeGameSeries;
+  final ValueSetter<String?> onChangeAmiiboSeries;
   final ValueSetter<String> onGoToDetail;
 
   @override
@@ -74,14 +89,17 @@ final class HomePageView extends HookWidget {
     ].contains(context.formFactor);
 
     final selectedType = useState<String?>(type);
-    final selectedSeries = useState<String?>(gameSeries);
+    final selectedGameSeries = useState<String?>(gameSeries);
+    final selectedAmiiboSeries = useState<String?>(amiiboSeries);
 
     useEffect(() {
       unawaited(context.read<GameSeriesCubit>().fetchGameSeries());
+      unawaited(context.read<AmiiboSeriesCubit>().fetchAmiiboSeries());
       unawaited(
         context.read<AmiiboListCubit>().fetchAmiiboData(
           type: selectedType.value,
-          gameSeries: selectedSeries.value,
+          gameSeries: selectedGameSeries.value,
+          amiiboSeries: selectedAmiiboSeries.value,
         ),
       );
       return null;
@@ -90,15 +108,44 @@ final class HomePageView extends HookWidget {
     final drawerMenu = DrawerMenu(
       onSelectType: (type) {
         selectedType.value = type;
-        _typeChange(context, type, selectedSeries.value);
+        _fetchAmiiboData(
+          context,
+          type: type,
+          gameSeries: selectedGameSeries.value,
+          amiiboSeries: selectedAmiiboSeries.value,
+        );
       },
       onSelectGameSeries: (series) {
-        selectedSeries.value = series;
-        _seriesChange(context, series, selectedType.value);
+        selectedGameSeries.value = series;
+        _fetchAmiiboData(
+          context,
+          type: selectedType.value,
+          gameSeries: series,
+          amiiboSeries: selectedAmiiboSeries.value,
+        );
       },
       makePop: !isDesktopOrTablet,
       selectedType: selectedType.value,
-      selectedGameSeries: selectedSeries.value,
+      selectedGameSeries: selectedGameSeries.value,
+    );
+
+    final mainContent = Column(
+      children: <Widget>[
+        FilterChipsSection(
+          selectedAmiiboSeries: selectedAmiiboSeries.value,
+          onSelectAmiiboSeries: (series) {
+            selectedAmiiboSeries.value = series;
+            onChangeAmiiboSeries(series);
+            _fetchAmiiboData(
+              context,
+              type: selectedType.value,
+              gameSeries: selectedGameSeries.value,
+              amiiboSeries: series,
+            );
+          },
+        ),
+        Expanded(child: _AmiiboList(onTapAmiibo: onGoToDetail)),
+      ],
     );
 
     return Scaffold(
@@ -114,30 +161,27 @@ final class HomePageView extends HookWidget {
         true => Row(
           children: <Widget>[
             Expanded(flex: 2, child: drawerMenu),
-            Expanded(flex: 5, child: _AmiiboList(onTapAmiibo: onGoToDetail)),
+            Expanded(flex: 5, child: mainContent),
           ],
         ),
-        _ => _AmiiboList(onTapAmiibo: onGoToDetail),
+        _ => mainContent,
       },
     );
   }
 
-  void _typeChange(BuildContext context, String? type, String? series) {
+  void _fetchAmiiboData(
+    BuildContext context, {
+    String? type,
+    String? gameSeries,
+    String? amiiboSeries,
+  }) {
     onChangeType(type);
+    onChangeGameSeries(gameSeries);
     unawaited(
       context.read<AmiiboListCubit>().fetchAmiiboData(
         type: type,
-        gameSeries: series,
-      ),
-    );
-  }
-
-  void _seriesChange(BuildContext context, String? series, String? type) {
-    onChangeGameSeries(series);
-    unawaited(
-      context.read<AmiiboListCubit>().fetchAmiiboData(
-        type: type,
-        gameSeries: series,
+        gameSeries: gameSeries,
+        amiiboSeries: amiiboSeries,
       ),
     );
   }
