22a2fad228218114f37078616a40e396966c2a67
FabianVarela <jfabian.303@gmail.com>
2026-05-03 13:55:55 -0500

    Update gradle and android version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 android {
     namespace = "com.developer.fabian.flutter_amiibo_responsive"
-    compileSdk = 35
-    ndkVersion = "27.0.12077973"
+    compileSdk = 36
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
