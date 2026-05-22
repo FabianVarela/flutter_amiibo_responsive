c2cf0f51ebdb053000f97e550c2d6801426387fc
FabianVarela <jfabian.303@gmail.com>
2026-05-21 22:21:11 -0500

    Replace window_size by window_manager

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
 import 'dart:developer';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_amiibo_responsive/utils/adaptive_contextual_layout.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
-import 'package:window_size/window_size.dart';
+import 'package:window_manager/window_manager.dart';
 
 final class AppBlocObserver extends BlocObserver {
   @override
@@ -32,11 +33,12 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
     usePathUrlStrategy();
     WidgetsFlutterBinding.ensureInitialized();
 
-    if (currentDevice == .desktop) {
-      setWindowTitle('Flutter Amiibo');
+    if (!kIsWeb && currentDevice == .desktop) {
+      await windowManager.ensureInitialized();
+      await windowManager.setTitle('Flutter Amiibo');
 
-      setWindowMinSize(const Size(300, 500));
-      setWindowMaxSize(const Size(1500, 900));
+      await windowManager.setMinimumSize(const Size(300, 500));
+      await windowManager.setMaximumSize(const Size(1500, 900));
     }
 
     runApp(await builder());
